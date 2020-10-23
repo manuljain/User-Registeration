61151f3106c98f701bec8af047188d6f08b19967
Manul <manuljain16@gmail.com>
2020-10-24 00:11:18 +0530

    Added rule 1 for password: minimum 8 characters

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,60 +5,60 @@ echo "Welcome to User Registration Program"
 
 shopt -s extglob
 
-# # Input first name
+# Input first name
 
-# regex="[[:upper:]]{1}[a-zA-Z]{2,17}$"
+regex="[[:upper:]]{1}[a-zA-Z]{2,17}$"
 
-# # Condition for checking first name is valid or not
-# while true
-# do
-#     read -p "Enter first name : " firstname
-#     if [[ $firstname =~ $regex ]]
-#     then
-#         echo "valid first name"
-#         break
-#     else
-#         echo "invalid first name"
-#         echo "HINT: The first letter should be uppercase and minimum length should be of 3 characters"
+# Condition for checking first name is valid or not
+while true
+do
+    read -p "Enter first name : " firstname
+    if [[ $firstname =~ $regex ]]
+    then
+        echo "valid first name"
+        break
+    else
+        echo "invalid first name"
+        echo "HINT: The first letter should be uppercase and minimum length should be of 3 characters"
 
-#     fi
-# done
-
-
-# # Condition for checking last name is valid or not
-# reg="[[:upper:]]{1}[a-zA-Z]{2,17}$"
-# while true
-# do
-#     # Input last name
-#     read -p "Enter last name : " lastname
-#     if [[ $lastname =~ $reg ]]
-#     then
-#         echo "valid last name"
-#         break
-#     else
-#         echo "invalid last name"
-#         echo "HINT: The last letter should be uppercase and minimum length should be of 3 characters"
-
-#     fi
-# done
+    fi
+done
 
 
-# # Condition for checking email address is valid or not
+# Condition for checking last name is valid or not
+reg="[[:upper:]]{1}[a-zA-Z]{2,17}$"
+while true
+do
+    # Input last name
+    read -p "Enter last name : " lastname
+    if [[ $lastname =~ $reg ]]
+    then
+        echo "valid last name"
+        break
+    else
+        echo "invalid last name"
+        echo "HINT: The last letter should be uppercase and minimum length should be of 3 characters"
+
+    fi
+done
 
 
-# regex_Em="[[:lower:]]+.[[:lower:]]+[0-9]+@[[:lower:]]+\.[[:lower:]]+.[[:lower:]]"
+# Condition for checking email address is valid or not
 
-# while true
-# do
-#     read -p "enter a valid email address : " emailid
-#     if [[ $emailid =~ $regex_Em ]]
-#     then
-#         echo "valid email id"
-#         break
-#     else
-#         echo "invalid email id"
-#     fi
-# done
+
+regex_Em="[[:lower:]]+.[[:lower:]]+@[[:lower:]]+\.[[:lower:]]+.[[:lower:]]"
+
+while true
+do
+    read -p "enter a valid email address : " emailid
+    if [[ $emailid =~ $regex_Em ]]
+    then
+        echo "valid email id"
+        break
+    else
+        echo "invalid email id"
+    fi
+done
 
 
 
@@ -77,5 +77,14 @@ do
         echo "invalid mobile number"
     fi
 done
-
-
+# Condition for checking password is valid or not
+# RULE 1: Minimum 8 characters
+read -p "enter a valid password : " passwd
+count=`echo ${#passwd}`
+if [[ $count -lt 8 ]]
+then
+    echo "Password length should be 8 character"
+    exit 1
+else
+    echo "Password is valid"
+fi
